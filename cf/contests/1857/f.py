29a5baa97c0a6ff4eb75a32e918958cd52d471e3
kamilszymczak1 <kamilkordianszymczak@gmail.com>
2023-08-08 18:50:32 +0200

    Fix the bug with problem F

--- cf/contests/1857/f.py
+++ cf/contests/1857/f.py
@@ -1,20 +1,38 @@
 import sys
-input = sys.stdin.readline
+from collections import Counter
 import math
 
+input = sys.stdin.readline
+
 def perfect_square(x):
-    y = int(round(math.sqrt(x)))
+    y = int(math.sqrt(x))
     return y * y == x
 
 for _ in range(int(input())):
     n = int(input())
     a = list(map(int, input().split()))
-    count = dict()
-    for x in a:
-        if x in count.keys():
-            count[x] += 1
-        else:
-            count[x] = 1
+    a.sort()
+    
+    def count_in_a(x):
+        l, r = 0, n - 1
+        while l < r:
+            m = (l + r) >> 1
+            if a[m] < x:
+                l = m + 1
+            else:
+                r = m
+        if l == n or a[l] != x:
+            return 0
+        p, q = l, n
+        while p < q:
+            m = (p + q) >> 1
+            if a[m] <= x:
+                p = m + 1
+            else:
+                q = m
+        return p - l
+            
+    
     
     for __ in range(int(input())):
         x, y = map(int, input().split())
@@ -22,12 +40,12 @@ for _ in range(int(input())):
         if x * x - 4 * y < 0 or not perfect_square(x * x - 4 * y):
             print("0", end = " ")
         else:
-            d = int(round(math.sqrt(x * x - 4 * y)))
+            d = int(math.sqrt(x * x - 4 * y))
             
             if d == 0:
-                c = count[x // 2]
+                c = count_in_a(x // 2)
                 print(c * (c - 1) // 2, end = " ")
             else:
-                print(count[(x - d) / 2] * count[(x + d) / 2], end = " ")
+                print(count_in_a((x - d) // 2) * count_in_a((x + d) // 2), end = " ")
         
     print("")
